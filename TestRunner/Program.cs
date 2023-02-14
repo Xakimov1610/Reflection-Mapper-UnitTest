3e2935869fb2c6da6ace61542a8fe8ff0d7802f4
Nosirbek Xakimov <nosirbekxakimov01@gmail.com>
2023-02-14 11:07:04 +0500

    Protses

--- TestRunner/Program.cs
+++ TestRunner/Program.cs
@@ -6,3 +6,12 @@ var testClasses = typeof(Program).Assembly.GetTypes()
 
 foreach (var testClass in testClasses)
     Console.WriteLine(testClass.FullName);
+
+
+foreach(Type testClass in testClasses)
+{
+    var testClassObj = Activator.CreateInstance(testClass);
+
+    var testMethods = testClass.GetMethods()
+        .Where(method => method.GetCustomAttribute(typeof(MyTestMethod)) != null);
+}
